3c2db5612460b653467664cdb94930f0404f85f4
BLACKMouha <mohamedsenghor05@gmail.com>
2023-07-24 13:51:39 +0000

    Turn 'add' to an arrow hold in 'addNeighborhood' into arrow funtion

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,7 @@
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+const res1 = neighborhoodsList.addNeighborhood('Black Might');
+console.log(res);
+console.log(res1);

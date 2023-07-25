86c9fa76e74e480834f81115d4fc346a0cbc6e3d
BLACKMouha <mohamedsenghor05@gmail.com>
2023-07-25 16:29:10 +0000

    Join all items of an iterator with a separator

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+    let string = '';
+    for (const employee of reportWithIterator) {
+        string += employee + '|';
+    }
+    return string.slice(0, -1);
+}

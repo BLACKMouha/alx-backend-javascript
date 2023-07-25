bf2cf7285ff80424e5e0ddf77761fb95499ce0e6
BLACKMouha <mohamedsenghor05@gmail.com>
2023-07-25 09:21:34 +0000

    ES6's for...of to loop through an array and change its items

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (let item of array.entries()) {
-      array[item[0]] = appendString + item[1];
+    for (let idx of array.keys()) {
+      const value = array[idx];
+      array[idx] = appendString + value;
     }
 
     return array;

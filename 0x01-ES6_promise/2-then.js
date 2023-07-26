566b743c12816f44d2376f931d8c5040924fa560
BLACKMouha <mohamedsenghor05@gmail.com>
2023-07-26 13:49:24 +0000

    Add a finally handler

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function getFullResponseFromAPI(promise) {
   const aPromise = new Promise((resolve, reject) => {
-    if (promise) resolve(promise); else reject(Error());
+    if (promise) resolve(({ status: 200, body: 'Success' })); else reject(Error());
   });
   return aPromise.finally(() => console.log('Got a response from the API'));
 }

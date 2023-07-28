1b0c81a2fb28f3760aa750c7e62bd01766e8a106
BLACKMouha <mohamedsenghor05@gmail.com>
2023-07-28 13:23:15 +0000

    Return an object which properties are set to null if one of the promise is rejected otherwise the value of the promises are set appropriately

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,10 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default async function asyncUploadUser() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((d) => {
-      for (const i of d) {
-        if (i.status === 'rejected') return { photo: null, user: null };
-      }
+  const u = await createUser();
+  const p = await uploadPhoto();
 
-      return { photo: d[0], user: d[1] };
-    });
+  u.then((d) => console.log(d));
+  p.then((d) => console.log(d));
+  return {};
 }

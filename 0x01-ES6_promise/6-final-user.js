a14f9b19b4639ee4a04a2888514b54fc6c0a59d0
BLACKMouha <mohamedsenghor05@gmail.com>
2023-07-28 10:05:44 +0000

    Returns a promised fully settled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,21 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
+
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((data) => data)
-    .catch((error) => error);
+    .then((d) => {
+      const r = [];
+
+      for (const i of d) {
+        if (i.status === 'fulfilled') {
+          r.push({ status: i.status, value: i.value });
+        } else {
+          r.push({
+            status: i.status,
+            value: `Error: ${filename} cannot be processed`
+          });
+        }
+      }
+      return r;
+    });
 }

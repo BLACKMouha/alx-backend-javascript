b5e284c065dcd689cac8217025374131e387fb02
BLACKMouha <mohamedsenghor05@gmail.com>
2023-07-30 16:38:54 +0000

    Building class constructed with a number, accessors

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,22 +1,29 @@
 export default class Building {
   constructor(sqft) {
     if (typeof (sqft) === 'number') {
+      // eslint-disable-next-line no-underscore-dangle
       this._sqft = sqft;
-    }
-    else throw new Error('Sqft must be a number');
+    } else throw new Error('Sqft must be a number');
+
     if (this.constructor !== Building) {
       throw new Error('Class extending Building must override evacuatiionWarningMessage');
     }
   }
 
   get sqft() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._sqft;
   }
 
   set sqft(value) {
-    if (typeof(value) === 'number') this._sqft = value;
+    // eslint-disable-next-line no-underscore-dangle
+    if (typeof (value) === 'number') this._sqft = value;
     else throw new Error('Sqft must be a number');
   }
 
-  evacuationWarningMessage() {}
+  evacuationWarningMessage() {
+    if (this.constructor !== Building) {
+      throw new Error('Class extending Building must override evacuatiionWarningMessage');
+    }
+  }
 }

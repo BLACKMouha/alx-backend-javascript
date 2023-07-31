361be85eb17a7958d36618722060272bc735bf0f
BLACKMouha <mohamedsenghor05@gmail.com>
2023-07-31 14:31:07 +0000

    Bugs corrected: hoisting classes, properties mispelling, constructor type checking

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -47,7 +47,7 @@ export class StudentHolberton {
 
   get fullStudentDescription() {
     // eslint-disable-next-line no-underscore-dangle
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass._year} - ${this._holbertonClass._location}`;
+    return `${this.fullName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 

7b3916059f726002fb7cdf149337ce3830a4b6ee
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-01 17:37:42 +0000

    updateUniqueItems update the Map passed in

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 export default function updateUniqueItems(aMap) {
   if (!(aMap instanceof Map)) throw new Error('Cannot process');
-  
+
   aMap.forEach((value, key) => {
     if (value === 1) {
       aMap.set(key, 100);

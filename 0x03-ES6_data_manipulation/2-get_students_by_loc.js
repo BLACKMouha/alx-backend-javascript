af28fa7b51b9715dd117776a594ad0115ad6af32
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-01 08:55:41 +0000

    getStudentsByLocation returns an array of objects who are located in a specific city

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,6 @@
 /* jshint esversion: 6 */
 
 export default function getStudentsByLocation(a, f) {
+  // eslint-disable-next-line arrow-parens
   return a instanceof Array && typeof f === 'string' ? a.filter(el => el.location === f) : [];
 }

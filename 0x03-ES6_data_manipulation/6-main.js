f3a6b0c84b4e6ef3ebe0839e30939bccbc3fd874
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-01 13:41:52 +0000

    setFromArray returns a Set from an array

---
+++ 0x03-ES6_data_manipulation/6-main.js
@@ -0,0 +1,5 @@
+/* jshint esversion: 6 */
+
+import setFromArray from './6-set';
+
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,5 @@
+/* jshint esversion: 6 */
+
+export default function setFromArray(arr) {
+  return new Set(arr);
+}

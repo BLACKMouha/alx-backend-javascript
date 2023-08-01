0c0636ddff1ece83445c5c5067e004a9e35b9170
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-01 15:21:40 +0000

    updateStudentGradeByCity returns an array of students for a specific city with their new grade.

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,10 @@
 /* jshint esversion: 6 */
 
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  // eslint-disable-next-line arrow-parens
-  return students.filter(student => student.location === city)
-    // eslint-disable-next-line arrow-parens
-    .toSorted((x, y) => x.id - y.id).map(student => {
-      // eslint-disable-next-line arrow-parens
-      newGrades.toSorted((a, b) => a.studentId - b.studentId).forEach(newGrade => {
-        // eslint-disable-next-line arrow-parens
-        if (newGrades.map(newGrade => newGrade.studentId).includes(student.id)) {
+  return students.filter((student) => student.location === city)
+    .toSorted((x, y) => x.id - y.id).map((student) => {
+      newGrades.toSorted((a, b) => a.studentId - b.studentId).forEach((newGrade) => {
+        if (newGrades.map((newGrade) => newGrade.studentId).includes(student.id)) {
           // eslint-disable-next-line no-param-reassign
           if (student.id === newGrade.studentId && !student.grade) student.grade = newGrade.grade ? newGrade.grade : 'N/A';
           // eslint-disable-next-line no-param-reassign

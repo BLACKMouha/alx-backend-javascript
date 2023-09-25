a566573b130bfbf24f17933a8d4b9c9f4853def7
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-25 22:57:39 +0000

    Create a small HTTP server with NodeJS' http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+/* jshint esversion: 8 */
+
+const http = require('node:http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json ' });
+  res.end(JSON.stringify({ data: 'Hello Holberton School!' }));
+});
+
+app.listen(1245);

411e8f7f5837e82afe78f778610a14d932a9964c
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-25 09:27:57 +0000

    Printing to STDOUT

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,12 +1,12 @@
-
 /* jshint esversion: 8 */
 const process = require('process');
 
 function displayMessage() {
   if (process.argv.length >= 3) {
-    process.stdout.write(process.argv[2] + '\n');
+    process.stdout.write(`${process.argv[2]}\n`);
   } else {
     process.stdout.write('Hello NodeJS!\n');
   }
 }
+
 displayMessage();

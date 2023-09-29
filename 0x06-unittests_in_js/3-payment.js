ada5825ddc789d7c9fbd8d2a1564d25d3811b078
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-29 16:19:11 +0000

    Using sinon.spy to track the usage of Utils.calculNumber in sendPaymentRequestToApi

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -5,7 +5,6 @@ const Utils = require("./utils");
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const sum = Utils.calculNumber('SUM', totalAmount, totalShipping)
   console.log(`The total is: ${sum}`);
-  return sum;
 };
 
 module.exports = sendPaymentRequestToApi;

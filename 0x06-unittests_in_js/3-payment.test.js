cacdd287643e56dbdfb06073eb36399b480f721b
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-29 17:01:33 +0000

    Using sinon.spy to track the usage of Utils.calculNumber in sendPaymentRequestToApi

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -6,25 +6,37 @@ const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', function () {
-  let utilsSpy;
+  let calculNumberSpy;
   let consoleSpy;
 
   beforeEach(() => {
-    utilsSpy = sinon.spy(Utils, 'calculNumber');
+    calculNumberSpy = sinon.spy(Utils, 'calculNumber');
     consoleSpy = sinon.spy(console, 'log');
   });
 
   afterEach(() => {
-    utilsSpy.restore();
+    calculNumberSpy.restore();
     consoleSpy.restore();
   });
 
   it("should inspect calling sendPaymentRequestToApi is the same as Utils.calculNumber('SUM', totalAmount, totalShipping')", function () {
     sendPaymentRequestToApi(100, 20);
-    expect(utilsSpy.calledOnceWith('SUM', 100, 20));
+    expect(calculNumberSpy.calledOnceWith('SUM', 100, 20)).to.be.true;
+    expect(calculNumberSpy('SUM', 100, 20)).to.equal(120);
   });
 
   it('logs to the console the right result of a call sendPaymentRequstToApi', function () {
     expect(consoleSpy.calledOnceWith('The total is: 120'));
+  });
+
+  it('should fail tests sendPaymentRequestToApi not using Utils.calculateNumber', function () {
+    const sendPaymentRequestToApiSpy = sinon.spy(
+      function sendPaymentRequestToAPI(totalAmount, totalShipping) {
+        const total = Math.round(totalAmount) + Math.round(totalShipping);
+        console.log(`The total is: ${total}`);
+      }
+    );
+    sendPaymentRequestToApiSpy(120, 30);
+    expect(calculNumberSpy.called).to.be.false;
   });
 });
